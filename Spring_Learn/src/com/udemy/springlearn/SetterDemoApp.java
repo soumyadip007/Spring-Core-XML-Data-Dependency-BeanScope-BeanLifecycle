eae6ec430a13a6df4d8dac6c0e9e9df80fdb662e
soumyadip007 <soumyadip.note@gmail.com>
2019-05-20 19:03:19 +0530

    Setter injection Dependency

---
+++ Spring_Learn/src/com/udemy/springlearn/SetterDemoApp.java
@@ -0,0 +1,19 @@
+package com.udemy.springlearn;
+
+public class SetterDemoApp {
+
+	public static void main(String[] args) {
+		
+		//Load  spring Configuration File
+		
+		
+		//Retrieve Bean from spring Container
+		
+	
+		//Call methods on the bean
+		
+		
+		//Close the context
+	}
+
+}
